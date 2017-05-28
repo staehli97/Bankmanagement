188f3283d114eac37ab60799e758cb69d2405394
Florian Bürgi <fb.buergi@gmx.ch>
2017-05-28 18:09:50 +0200

    fix app init flo

--- src/javaapplication/BankenManagement.java
+++ src/javaapplication/BankenManagement.java
@@ -9,9 +9,10 @@ public class BankenManagement {
 
         GUI_Main b1 = new GUI_Main();
         b1.setSize(600, 600);
-        b1.setVisible(true);
 
         Panel_Beispiel b2 = new Panel_Beispiel();
         b1.add(b2);
+                b1.setVisible(true);
+
     }
 }
